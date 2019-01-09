a4de4ca8da1a9b4ba04842340c47a2bb6ad8156c
noname9xndz <44211401+noname9xndz@users.noreply.github.com>
2019-01-09 19:20:16 +0700

    fix bug hiển thị dữ liệu webapi(productCategoryList)

--- Shop2.Web/app/components/product_categories/productCategoryListController.js
+++ Shop2.Web/app/components/product_categories/productCategoryListController.js
@@ -4,7 +4,21 @@
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    function productCategoryListController() {
+    productCategoryListController.$inject = ['$scope','apiService'];
+    function productCategoryListController($scope,apiService) {
+        $scope.productCategories = [];
+
+        // lấy dữ liệu từ service
+        $scope.getProductCategories = getProductCategories;
+
+        function getProductCategories() {
+            apiService.get('/api/productcategory/getall', null, function (result) {
+                $scope.productCategories = result.data;
+            }, function () {
+                console.log('load productcategory failed');
+                });
+        }
+        $scope.getProductCategories();
 
     }
 })(angular.module('shop2.productCategories')); // chỉ ra controller này thuộc module nào
